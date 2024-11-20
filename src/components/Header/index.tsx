275ad0d006743ac69aede3927d22ed028af7f28e
Kelvon <kelvonlys@gmail.com>
2024-11-20 14:43:24 +0800

    fixed error of jsvectormap, changed layout and description of dashboard

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -71,14 +71,14 @@ const Header = (props: {
             <h1 className="mb-0.5 text-heading-5 font-bold text-dark dark:text-white">
               Dashboard
             </h1>
-            <p className="font-medium">Next.js Admin Dashboard Solution</p>
+            <p className="font-medium">Manager your trades here</p>
           </div>
         </div>
 
         <div className="flex items-center justify-normal gap-2 2xsm:gap-4 lg:w-full lg:justify-between xl:w-auto xl:justify-normal">
           <ul className="flex items-center gap-2 2xsm:gap-4">
             {/* <!-- Search Form --> */}
-            <SearchForm />
+            {/* <SearchForm /> */}
             {/* <!-- Search Form --> */}
 
             {/* <!-- Dark Mode Toggle --> */}
@@ -86,12 +86,12 @@ const Header = (props: {
             {/* <!-- Dark Mode Toggle --> */}
 
             {/* <!-- Notification Menu Area --> */}
-            <DropdownNotification />
+            {/* <DropdownNotification /> */}
             {/* <!-- Notification Menu Area --> */}
           </ul>
 
           {/* <!-- User Area --> */}
-          <DropdownUser />
+          {/* <DropdownUser /> */}
           {/* <!-- User Area --> */}
         </div>
       </div>
