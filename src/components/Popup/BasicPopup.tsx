cb5a73fe8ee110cacc3e68fd6ff0ddd39aefd276
Kelvon <kelvonlys@gmail.com>
2024-12-11 18:59:00 +0800

    added toogle style and change to black background

--- src/components/Popup/BasicPopup.tsx
+++ src/components/Popup/BasicPopup.tsx
@@ -33,79 +33,86 @@ const PopupBox: React.FC<PopupBoxProps> = ({ isOpen, onClose }) => {
 
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-50 z-999">
-      <div ref={popupRef} className="bg-white p-10 rounded-lg w-1/3 sm:w-2/3 md:w-1/3 min-w-[80%] md:min-w-[400px]">
+      <div ref={popupRef} className="bg-black p-7 pt-8 rounded-lg w-1/3 sm:w-2/3 md:w-1/3 min-w-[80%] md:min-w-[400px]">
         <div className="flex justify-between items-center mb-8">
-          <h2 className="text-xl font-bold">Manage Position</h2>
-          <button onClick={onClose} className="text-gray-500 hover:text-gray-700">
+          <h2 className="text-xl font-bold text-gray-200">Manage Position</h2>
+          <button onClick={onClose} className="text-gray-400 hover:text-gray-200">
             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
             </svg>
           </button>
         </div>
-        
+
         {/* Take Profit Section */}
-        <div className="mb-4">
-          <label className="flex items-center">
-            <input 
-              type="checkbox" 
-              className="mr-2" 
-              checked={isTakeProfitChecked}
-              onChange={(e) => setIsTakeProfitChecked(e.target.checked)}
-            />
-            <span>Take Profit</span>
+        <div className="mb-4 flex items-center justify-between">
+          <label className="flex items-center text-gray-400">
+            <div className="relative">
+              <input 
+                type="checkbox" 
+                className="sr-only"
+                checked={isTakeProfitChecked}
+                onChange={(e) => setIsTakeProfitChecked(e.target.checked)}
+              />
+              <div className={`block w-10 h-6 rounded-full ${isTakeProfitChecked ? 'bg-blue-600' : 'bg-gray-600'}`}></div>
+              <div className={`dot absolute left-1 top-1 bg-white w-4 h-4 rounded-full transition ${isTakeProfitChecked ? 'transform translate-x-full' : ''}`}></div>
+            </div>
+            <span className="ml-3">Take Profit</span>
           </label>
         </div>
 
         {isTakeProfitChecked && (
           <>
             <div className="flex mb-1">
-              <label className="block mb-2">Take Profit</label>
+              <label className="block mb-2 text-gray-400">Take Profit</label>
             </div>
 
             <div className="flex mb-10 space-x-7">
-              <input type="text" className="w-1/2 px-3 py-3 border rounded-lg" placeholder="Price" />
-              <input type="text" className="w-1/2 px-3 py-3 border rounded-lg" placeholder="Pips" />
+              <input type="text" className="w-1/2 px-3 py-3 border rounded-lg bg-gray-900 text-gray-200 border-gray-700 placeholder-gray-500" placeholder="Price" />
+              <input type="text" className="w-1/2 px-3 py-3 border rounded-lg bg-gray-900 text-gray-200 border-gray-700 placeholder-gray-500" placeholder="Pips" />
             </div>
           </>
         )}
 
         {/* Stop Loss Section */}
-         <div className="mb-4">
-          <label className="flex items-center">
-            <input 
-              type="checkbox" 
-              className="mr-2" 
-              checked={isStopLossChecked}
-              onChange={(e) => setIsStopLossChecked(e.target.checked)}
-            />
-            <span>Stop Loss</span>
+        <div className="mb-4 flex items-center justify-between">
+          <label className="flex items-center text-gray-400">
+            <div className="relative">
+              <input 
+                type="checkbox" 
+                className="sr-only"
+                checked={isStopLossChecked}
+                onChange={(e) => setIsStopLossChecked(e.target.checked)}
+              />
+              <div className={`block w-10 h-6 rounded-full ${isStopLossChecked ? 'bg-blue-600' : 'bg-gray-600'}`}></div>
+              <div className={`dot absolute left-1 top-1 bg-white w-4 h-4 rounded-full transition ${isStopLossChecked ? 'transform translate-x-full' : ''}`}></div>
+            </div>
+            <span className="ml-3">Stop Loss</span>
           </label>
         </div>
 
         {isStopLossChecked && (
           <>
-            <div className="mb-4">
-              <label className="block mb-2">Stop Loss</label>
+            <div className="flex mb-4">
+              <label className="block mb-2 text-gray-400">Stop Loss</label>
             </div>
             <div className="flex mb-10 space-x-7">
-              <input type="text" className="w-1/2 px-3 py-3 border rounded-lg" placeholder="Price" />
-              <input type="text" className="w-1/2 px-3 py-3 border rounded-lg" placeholder="Pips" />
+              <input type="text" className="w-1/2 px-3 py-3 border rounded-lg bg-gray-900 text-gray-200 border-gray-700 placeholder-gray-500" placeholder="Price" />
+              <input type="text" className="w-1/2 px-3 py-3 border rounded-lg bg-gray-900 text-gray-200 border-gray-700 placeholder-gray-500" placeholder="Pips" />
             </div>
           </>
         )}
-
-        {/* Bottom Row: Save and Cancel buttons */}
-        <div className="flex space-x-2">
-          <ButtonDefault
-            label="Close"
-            onClick={onClose}
-            customClasses="bg-gray-400 text-white px-5 py-2.5 text-sm"
-          />
-          <ButtonDefault
-            label="Save"
-            onClick={onClose}
-            customClasses="bg-blue-700 text-white px-4 py-2.5 text-sm"
-          />
+              
+        {/* Buy or Sell Buttons */}
+        <div className="mb-1">
+          <label className="block mb-2 text-gray-400">Side *</label>
+          <div className="flex space-x-0.5">
+            <button
+              className="w-full px-3 py-3 rounded-2xl bg-blue-700 text-gray-200 hover:bg-opacity-80"
+            >
+              <div>Modify</div>
+            </button>
+            
+          </div>
         </div>
       </div>
     </div>
--- src/components/Popup/TradePopup.tsx
+++ src/components/Popup/TradePopup.tsx
@@ -9,7 +9,6 @@ interface TradePopupBoxProps {
 const TradePopupBox: React.FC<TradePopupBoxProps> = ({ isOpen, onClose }) => {
   const [isTakeProfitChecked, setIsTakeProfitChecked] = useState(true);
   const [isStopLossChecked, setIsStopLossChecked] = useState(true);
-  const [selectedSide, setSelectedSide] = useState('buy');
   const popupRef = useRef<HTMLDivElement>(null);
 
   const handleClickOutside = (event: MouseEvent) => {
@@ -34,10 +33,10 @@ const TradePopupBox: React.FC<TradePopupBoxProps> = ({ isOpen, onClose }) => {
 
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-50 z-999">
-      <div ref={popupRef} className="bg-white p-10 rounded-lg w-1/3 sm:w-2/3 md:w-1/3 min-w-[80%] md:min-w-[400px]">
+      <div ref={popupRef} className="bg-black p-7 pt-8 rounded-lg w-1/3 sm:w-2/3 md:w-1/3 min-w-[80%] md:min-w-[400px]">
         <div className="flex justify-between items-center mb-8">
-          <h2 className="text-xl font-bold">Manage Position</h2>
-          <button onClick={onClose} className="text-gray-500 hover:text-gray-700">
+          <h2 className="text-xl font-bold text-gray-200">Manage Position</h2>
+          <button onClick={onClose} className="text-gray-400 hover:text-gray-200">
             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
             </svg>
@@ -46,76 +45,86 @@ const TradePopupBox: React.FC<TradePopupBoxProps> = ({ isOpen, onClose }) => {
         
         {/* Position Size */}
         <div className="mb-4">
-          <label className="block mb-2 text-gray-500">Position Size (Lots) *</label>
-          <input type="text" className="w-full px-3 py-3 border rounded-lg" placeholder="Enter Position Size" />
+          <label className="block mb-2 text-gray-400">Position Size (Lots) *</label>
+          <input type="text" className="w-full px-3 py-3 border rounded-lg bg-gray-900 text-gray-200 border-gray-700 placeholder-gray-500" placeholder="Enter Position Size" />
         </div>
 
         {/* Take Profit Section */}
-        <div className="mb-4">
-          <label className="flex items-center">
-            <input 
-              type="checkbox" 
-              className="mr-2" 
-              checked={isTakeProfitChecked}
-              onChange={(e) => setIsTakeProfitChecked(e.target.checked)}
-            />
-            <span>Take Profit</span>
+        <div className="mb-4 flex items-center justify-between">
+          <label className="flex items-center text-gray-400">
+            <div className="relative">
+              <input 
+                type="checkbox" 
+                className="sr-only"
+                checked={isTakeProfitChecked}
+                onChange={(e) => setIsTakeProfitChecked(e.target.checked)}
+              />
+              <div className={`block w-10 h-6 rounded-full ${isTakeProfitChecked ? 'bg-blue-600' : 'bg-gray-600'}`}></div>
+              <div className={`dot absolute left-1 top-1 bg-white w-4 h-4 rounded-full transition ${isTakeProfitChecked ? 'transform translate-x-full' : ''}`}></div>
+            </div>
+            <span className="ml-3">Take Profit</span>
           </label>
         </div>
 
         {isTakeProfitChecked && (
           <>
             <div className="flex mb-1">
-              <label className="block mb-2">Take Profit</label>
+              <label className="block mb-2 text-gray-400">Take Profit</label>
             </div>
 
             <div className="flex mb-10 space-x-7">
-              <input type="text" className="w-1/2 px-3 py-3 border rounded-lg" placeholder="Price" />
-              <input type="text" className="w-1/2 px-3 py-3 border rounded-lg" placeholder="Pips" />
+              <input type="text" className="w-1/2 px-3 py-3 border rounded-lg bg-gray-900 text-gray-200 border-gray-700 placeholder-gray-500" placeholder="Price" />
+              <input type="text" className="w-1/2 px-3 py-3 border rounded-lg bg-gray-900 text-gray-200 border-gray-700 placeholder-gray-500" placeholder="Pips" />
             </div>
           </>
         )}
 
         {/* Stop Loss Section */}
-        <div className="mb-4">
-          <label className="flex items-center">
-            <input 
-              type="checkbox" 
-              className="mr-2" 
-              checked={isStopLossChecked}
-              onChange={(e) => setIsStopLossChecked(e.target.checked)}
-            />
-            <span>Stop Loss</span>
+        <div className="mb-4 flex items-center justify-between">
+          <label className="flex items-center text-gray-400">
+            <div className="relative">
+              <input 
+                type="checkbox" 
+                className="sr-only"
+                checked={isStopLossChecked}
+                onChange={(e) => setIsStopLossChecked(e.target.checked)}
+              />
+              <div className={`block w-10 h-6 rounded-full ${isStopLossChecked ? 'bg-blue-600' : 'bg-gray-600'}`}></div>
+              <div className={`dot absolute left-1 top-1 bg-white w-4 h-4 rounded-full transition ${isStopLossChecked ? 'transform translate-x-full' : ''}`}></div>
+            </div>
+            <span className="ml-3">Stop Loss</span>
           </label>
         </div>
 
         {isStopLossChecked && (
           <>
             <div className="flex mb-4">
-              <label className="block mb-2">Stop Loss</label>
+              <label className="block mb-2 text-gray-400">Stop Loss</label>
             </div>
             <div className="flex mb-10 space-x-7">
-              <input type="text" className="w-1/2 px-3 py-3 border rounded-lg" placeholder="Price" />
-              <input type="text" className="w-1/2 px-3 py-3 border rounded-lg" placeholder="Pips" />
+              <input type="text" className="w-1/2 px-3 py-3 border rounded-lg bg-gray-900 text-gray-200 border-gray-700 placeholder-gray-500" placeholder="Price" />
+              <input type="text" className="w-1/2 px-3 py-3 border rounded-lg bg-gray-900 text-gray-200 border-gray-700 placeholder-gray-500" placeholder="Pips" />
             </div>
           </>
         )}
               
         {/* Buy or Sell Buttons */}
-        <div className="mb-4">
-          <label className="block mb-2 text-gray-500">Side *</label>
-          <div className="flex">
-            <ButtonDefault
-            label="Buy"
-            customClasses="w-1/2 bg-blue-700 text-white px-5 py-2.5 text-sm"
-            />
-            <ButtonDefault
-            label="Sell"
-            customClasses="w-1/2 bg-red-600 text-white px-5 py-2.5 text-sm"
-            />
+        <div className="mb-1">
+          <div className="flex space-x-0.5">
+            <button
+              className="w-1/2 px-3 py-2 rounded-l-2xl border-r-0 bg-blue-500 text-gray-200 hover:bg-blue-700"
+            >
+              <div>Buy</div>
+              <div>1.050309</div>
+            </button>
+            <button
+              className="w-1/2 px-3 py-2 rounded-r-2xl border-l-0 bg-red-400 text-gray-200 hover:bg-red-700"
+            >
+              <div>Sell</div>
+              <div>1.050309</div>
+            </button>
           </div>
         </div>
-        
       </div>
     </div>
   );
